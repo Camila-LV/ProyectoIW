d5e0f9e35dced3e0ff20819a9ec93cfc289e98fd
Camila-LV <120059189+Camila-LV@users.noreply.github.com>
2023-01-06 13:07:32 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,25 @@
+function loguear() {
+
+  let user = document.getElementById("logemail").value;
+  let pass = document.getElementById("logpass").value;
+
+  if (user == "vallisguerrero32@gmail.com" && pass == "12345678") {
+
+    window.location = "Principal-page.html";
+  }
+
+  else {
+    alert("Datos incorrectos");
+
+  }
+  
+  if (user == "vallisguerrero32@gmail.com" && pass == "12345678") {
+
+    window.location = "Principal-page.html";
+  }
+
+  else {
+    alert("Datos incorrectos");
+
+  }
+}
